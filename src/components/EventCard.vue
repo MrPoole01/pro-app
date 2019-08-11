22b056f2ce86437af12a6c4532da42470337475c
Recardo Poole <recardo.poole@gmail.com>
2019-08-11 17:59:16 -0600

    card creation commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <router-link class="event-link" :to="{name: 'event-show', params: { id: '1'}}">
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time}} at {{ event.date}}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendies.length }} attending</span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: "Park Cleanup",
+        date: "Sun Aug 11, 2019",
+        time: "6:00",
+        attendies: [
+          { id: "123abc", name: "Darnell Jones" },
+          { id: "abc123", name: "Monica Davis" }
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
