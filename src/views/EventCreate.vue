a373088c9b292aeae8e284cb2b4760f05536f39c
Recardo Poole <recardo.poole@gmail.com>
2019-08-13 17:03:23 -0600

    Vuex state management setup

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <h4>There are {{ catLength }} catagories in this event!</h4>
+    <p v-for="cat in categories" :key="cat">{{ cat }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["user", "categories"]),
+    ...mapGetters(["catLength"])
+  }
+};
+</script>
